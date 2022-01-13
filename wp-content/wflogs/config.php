a2a8dd271a7accacbf3a933feb35fced00c1da51
Arthur JORIS <72022883+LordAythur@users.noreply.github.com>
2022-01-13 22:56:19 +0100

    Plugins

---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1642715564;s:7:"authKey";s:64:"nV$uUrM`9X58o% %uln#*uPZUgh{=ZTp2rquM..4_l,N/*O^>jE}%6tE%bTAC(u=";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:819;}
